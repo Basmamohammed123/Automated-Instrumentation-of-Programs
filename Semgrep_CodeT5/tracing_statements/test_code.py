94c72dee4b3effd0a508febf808c67aa9673333d
Viraj <virajpatel3@cmail.carleton.ca>
2025-01-22 00:14:24 -0500

    Controller

---
+++ Semgrep_CodeT5/tracing_statements/test_code.py
@@ -0,0 +1,8 @@
+#sample code
+# Add a to the current number.
+def add(a):
+# def _get_next_int ( self, a ) : if a < 5 : return a + 2 elif a > 5 : return a + 2 elif a < 5 : return a + 4 else : return a
+    if a > 5:
+        return a + 2
+    else:
+        return a + 4
