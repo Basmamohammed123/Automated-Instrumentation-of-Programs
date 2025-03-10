54f6edc9c83477e4c0712ffb7ab5ebe26ee1e037
Viraj <virajpatel3@cmail.carleton.ca>
2025-03-10 13:57:09 -0400

    Save file function

--- Semgrep_CodeT5/tracing_statements/file_loader.py
+++ Semgrep_CodeT5/tracing_statements/file_loader.py
@@ -2,6 +2,7 @@ import tkinter as tk
 from tkinter import filedialog
 import shutil
 import os
+from tkinter import messagebox
 
 
 def get_file_path():
@@ -24,6 +25,37 @@ def get_file_path():
 
             with open(test_code_path, 'w') as test_code_file:
                 test_code_file.write(file_contents)
+
+            print(f"Contents from {file_path} have been copied to {test_code_path}")
+            return file_path
+        except Exception as e:
+            print(f"Error: {e}")
+    else:
+        print("No file selected.")
+        return None
+
+def save_file():
+    print("Save check")    # REMOVE
+    root = tk.Tk()
+    root.withdraw()
+
+    response = messagebox.askyesno("Save File", "Do you want to save the file?")
+    if not response:
+        return
+
+    file_path = filedialog.askopenfilename(title="Select a file")
+    if file_path:
+
+        script_dir = os.path.dirname(os.path.abspath(__file__))
+
+        test_code_path = os.path.join(script_dir, 'test_code.py')
+
+        try:
+            with open(test_code_path, 'r') as test_code_file:
+                file_contents = test_code_file.read()
+
+            with open(file_path, 'w') as source_file:
+                source_file.write(file_contents)
 
             print(f"Contents from {file_path} have been copied to {test_code_path}")
             return file_path
